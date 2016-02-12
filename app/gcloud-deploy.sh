92c5230a0e3dd847fdea8bddfcff8dc7fa89fb1b
jefBinomed <jean.francois.garreau@gmail.com>
2016-02-12 21:51:52 +0100

    recopie des fichiers de devfest 2015 qui serviront pour 2016

---
+++ app/gcloud-deploy.sh
@@ -0,0 +1,6 @@
+google-cloud-sdk/bin/gcloud config set app/use_appengine_api false
+echo $GAE_KEY_FILE_CONTENT > file.key
+echo "Authenticate with $GAE_SERVICE_ACCOUNT"
+google-cloud-sdk/bin/gcloud auth activate-service-account $GAE_SERVICE_ACCOUNT --key-file file.key
+echo "Deploying to devfest"
+google-cloud-sdk/bin/gcloud --project devfestnantes2016 preview app deploy --version v1 --quiet dist/app.yaml
