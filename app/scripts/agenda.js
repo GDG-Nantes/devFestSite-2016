18150e7cd290e82e5cef450f1a97a75a841dcc5c
Benjamin <bpetetot@gmail.com>
2016-08-21 23:12:48 +0200

    test 10 (pour julien :) - finalisation de l'agenda

--- app/scripts/agenda.js
+++ app/scripts/agenda.js
@@ -2,14 +2,28 @@
 var agendaVue = new Vue({
   el: '#agendaVue',
   data: {
+    displayedSessions: null,
     sessions: null,
     speakers: null,
     agenda: null,
-    favorites: []
+    favorites: [],
+    typeFilters: ['conference', 'codelab', 'quickie'],
+    curTypeFilters: [],
+    trackFilters: ['web', 'cloud', 'mobile', 'discovery'],
+    curTrackFilters: [],
+    curFavoriteFilter: [],
+    day1: true,
+    day2: true
   },
 
   ready: function () {
     this.fetchData()
+  },
+
+  watch: {
+    curTypeFilters: 'filter',
+    curTrackFilters: 'filter',
+    curFavoriteFilter: 'filter'
   },
 
   methods: {
@@ -19,21 +33,46 @@ var agendaVue = new Vue({
         return response.json();
       }).then(function(json) {
         self.sessions = json.sessions;
+        self.displayedSessions = json.sessions;
         self.speakers = json.speakers;
         self.agenda = json.agenda;
       });
 
-      fetch('api/v1/stars/get?login=ben').then(function(response) {
-        return response.json();
-      }).then(function(json) {
-        self.favorites = json.favs;
-      });
+      self.favorites = JSON.parse(localStorage['fav']) || []
+      //fetch('api/v1/stars/get?login=ben').then(function(response) {
+      //  return response.json();
+      //}).then(function(json) {
+      //  self.favorites = json.favs;
+      //});
+    },
+    getTypeColor: getTypeColor,
+    getTrackColor: getTrackColor,
+    filter: function() {
+      var self = this;
+      var result = this.sessions;
+      if (this.curTypeFilters.length > 0) {
+        result = result.filter(function(session) {
+          return self.curTypeFilters.indexOf(session.type) !== -1
+        })
+      }
+      if (this.curTrackFilters.length > 0) {
+        result = result.filter(function(session) {
+          return self.curTrackFilters.indexOf(session.track) !== -1
+        })
+      }
+      if (this.curFavoriteFilter.length > 0) {
+        result = result.filter(function(session) {
+          return self.favorites.indexOf(session.id.toString()) !== -1
+        })
+      }
+      this.displayedSessions = result;
     }
   },
 
   events: {
     'toggle-favorite': function(id, favorite) {
       toggleFavorite(id, favorite, this.favorites)
+      this.filter()
     }
   }
 });
