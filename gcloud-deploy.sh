222804096b6670c9059fbaa7d82725b244a77f87
Julien Landuré <julien.landure@gmail.com>
2016-09-21 11:09:05 +0200

    chore(version): ready for next release

--- gcloud-deploy.sh
+++ gcloud-deploy.sh
@@ -5,4 +5,4 @@ echo "Authenticate with $GAE_SERVICE_ACCOUNT"
 google-cloud-sdk/bin/gcloud auth activate-service-account $GAE_SERVICE_ACCOUNT --key-file file.key
 echo "Deploying to devfest"
 ls -l
-google-cloud-sdk/bin/gcloud --project devfestnantes2016 preview app deploy --version v14 --quiet dist/app.yaml
+google-cloud-sdk/bin/gcloud --project devfestnantes2016 preview app deploy --version v15 --quiet dist/app.yaml
