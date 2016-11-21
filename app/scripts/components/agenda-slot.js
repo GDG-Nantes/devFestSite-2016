c4f360a2a47e2ae3be80f2a61c0d5fdbb14c7bd3
Julien Landuré <julien.landure@gmail.com>
2016-11-21 14:54:43 +0100

    feat(#146): add videos

--- app/scripts/components/agenda-slot.js
+++ app/scripts/components/agenda-slot.js
@@ -1,11 +1,11 @@
 /* eslint-disable */
 const agendaSlotTemplate =
-'<a class="devfest-agenda-card" href="./session.html?id={{session.id}}">'
+'<div class="devfest-agenda-card">'
 + '<div class="devfest-agenda-card-avatars mdl-cell--hide-phone">'
 + '  <img v-if="session.image" :src="session.image" class="devfest-agenda-card-img" />'
 + '  <img v-if="session.speaker" v-for="speaker in session.speaker" :src="this.getSpeakerPhoto(speaker, this.$root.speakers)" class="devfest-agenda-card-img" />'
 + '</div>'
-+ '<div class="devfest-agenda-card-content">'
++ '<a class="devfest-agenda-card-content" href="./session.html?id={{session.id}}">'
 + '  <span class="devfest-agenda-card-content-session mdl-typography--font-light">{{ session.name }}</span>'
 + '  <div class="devfest-agenda-card-content-speaker  mdl-typography--font-light" v-if="session.speaker">'
 + '    {{ this.getSpeakers(session.speaker, this.$root.speakers) }}'
@@ -16,9 +16,11 @@ const agendaSlotTemplate =
 + '    <span v-if="session.type" class="devfest-chip color-bg-default">{{this.getTimeLabel(session.type)}}</span>'
 + '    <span v-if="session.agenda.room" class="devfest-chip color-bg-default">{{this.getRoomLabel(session.agenda.room) }}</span>'
 + '  </div>'
-+ '</div>'
-+ '<div style="align-self:flex-start"><favorite :sid="session.id" :active="isFavorite(session.id, favorites)" ></favorite></div>'
 + '</a>'
++ '<div style="align-self:flex-start"><favorite :sid="session.id" :active="isFavorite(session.id, favorites)" ></favorite></div>'
++ '<div style="align-self:flex-start" v-if="session.videoUrl"><a href={{session.videoUrl}} target="_NEW"><i class="material-icons">videocam</i></a></div>'
++ '</div>'
+
 
 var AgendaSlot = Vue.extend({
   props: {
