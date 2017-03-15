e56a5a671f3f67d23a90683cc6b77eb6f3878ccf
Julien Landuré <julien.landure@gmail.com>
2017-03-15 17:42:12 +0100

    :sparkles: new schedule

--- gcloud-deploy.sh
+++ gcloud-deploy.sh
@@ -4,4 +4,4 @@ echo $GAE_KEY_FILE_CONTENT > file.key
 echo "Authenticate with $GAE_SERVICE_ACCOUNT"
 google-cloud-sdk/bin/gcloud auth activate-service-account $GAE_SERVICE_ACCOUNT --key-file file.key
 echo "Deploying to tz-nantes"
-google-cloud-sdk/bin/gcloud --project tz-nantes preview app deploy --version v1 --quiet dist/app.yaml
+google-cloud-sdk/bin/gcloud --project tz-nantes app deploy --version v2 --quiet dist/app.yaml
